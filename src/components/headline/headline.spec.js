8a0d19d3437edc8b3d4eddea117acedb6373a150
oliburt <oliburt@gmail.com>
2019-12-16 23:41:23 +0000

    Adding PropTypes to Headline component

--- src/components/headline/headline.spec.js
+++ src/components/headline/headline.spec.js
@@ -1,14 +1,36 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Headline from "./index";
-import { findByDataTestAttribute } from "../../../utils";
+import { findByDataTestAttribute, checkProps } from "../../../utils";
 
 const setUp = (props = {}) => {
   return shallow(<Headline {...props} />);
 };
 
 describe("Headline Component", () => {
-  describe("Have props", () => {
+  
+  
+  describe('Checking PropTypes', () => {
+      it('Should not throw a warning', () => {
+          const expectedProps = {
+              header: 'Test header',
+              desc: 'test desc',
+              tempArr: [{
+                  fName: 'test fname',
+                  lname: 'test lname',
+                  email: 'test email',
+                  age: 23,
+                  onlineStatus: false,
+              }]
+          }
+          const propsErr = checkProps(Headline, expectedProps);
+          expect(propsErr).toBeUndefined();
+      })
+  })
+  
+  
+  
+    describe("Have props", () => {
     let wrapper;
     beforeEach(() => {
       const props = {
@@ -18,30 +40,30 @@ describe("Headline Component", () => {
       wrapper = setUp(props);
     });
 
-    it('Should render without errors', () => {
-        const component = findByDataTestAttribute(wrapper, 'HeadlineComponent')
-        expect(component).toHaveLength(1)
-    })
+    it("Should render without errors", () => {
+      const component = findByDataTestAttribute(wrapper, "HeadlineComponent");
+      expect(component).toHaveLength(1);
+    });
 
-    it('Should render an h1', () => {
-        const h1 = findByDataTestAttribute(wrapper, 'header')
-        expect(h1).toHaveLength(1)
-    })
-    it('Should render a desc', () => {
-        const desc = findByDataTestAttribute(wrapper, 'desc')
-        expect(desc).toHaveLength(1)
-    })
+    it("Should render an h1", () => {
+      const h1 = findByDataTestAttribute(wrapper, "header");
+      expect(h1).toHaveLength(1);
+    });
+    it("Should render a desc", () => {
+      const desc = findByDataTestAttribute(wrapper, "desc");
+      expect(desc).toHaveLength(1);
+    });
   });
 
   describe("Have no props", () => {
-      let wrapper;
-      beforeEach(() => {
-          wrapper = setUp();
-      })
+    let wrapper;
+    beforeEach(() => {
+      wrapper = setUp();
+    });
 
-      it('Should not render', () => {
-          const component = findByDataTestAttribute(wrapper, 'HeadlineComponent')
-          expect(component).toHaveLength(0);
-      })
+    it("Should not render", () => {
+      const component = findByDataTestAttribute(wrapper, "HeadlineComponent");
+      expect(component).toHaveLength(0);
+    });
   });
 });
--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,9 @@
+import checkPropTypes from "check-prop-types";
+
 export const findByDataTestAttribute = (component, attr) => {
     return component.find(`[data-test='${attr}']`);
+}
+
+export const checkProps = (component, expectedProps) => {
+    return checkPropTypes(component.propTypes, expectedProps, 'props', component.name)
 }
