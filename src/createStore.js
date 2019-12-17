9e65a549692c88af4c86de2d95f9da0e9d358214
oliburt <oliburt@gmail.com>
2019-12-17 14:23:14 +0000

    filename change import update

--- src/createStore.js
+++ src/createStore.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import ReduxThunk from "redux-thunk";
-import RootReducer from "./reducers/rootReducer";
+import RootReducer from "./reducers";
 
 
 export const middlewares = [ReduxThunk];
