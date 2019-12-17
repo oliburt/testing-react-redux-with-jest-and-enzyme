d59ba9f3fdceb0b50e42c9fb2515f1d6200200a0
oliburt <oliburt@gmail.com>
2019-12-17 14:11:26 +0000

    added redux to app

---
+++ src/createStore.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from "redux";
+import ReduxThunk from "redux-thunk";
+import RootReducer from "./reducers/rootReducer";
+
+
+export const middlewares = [ReduxThunk];
+
+export const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore)
+
+export const store = createStoreWithMiddleware(RootReducer);
